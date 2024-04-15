c9832cc58eb76a6238803813072723a4f54324bc
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2024-04-15 06:55:50 -0700

    Completes Challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const imgContainer = document.getElementById('dog-image-container');
+
+fetch(imgUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new error('Network response failed');
+    }
+    return response.json();
+  })
+  .then((images) => {
+    images.message.forEach((image) => {
+      const dogImage = document.createElement('img');
+      dogImage.src = image;
+      imgContainer.append(dogImage);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    const errorElement = document.createTextNode(
+      'Error loading images, please refresh.'
+    );
+    imgContainer.append(errorElement);
+  });
